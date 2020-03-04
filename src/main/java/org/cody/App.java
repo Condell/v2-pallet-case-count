fde4c21220ea6977b5aeff8a6841186413556106
condell <cjewell468@gmail.com>
2020-03-04 15:48:48 -0500

    Correctly parse part of the pdf.

--- src/main/java/org/cody/App.java
+++ src/main/java/org/cody/App.java
@@ -10,9 +10,7 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Hello world!
- */
+
 public class App {
   public static void main(String[] args) {
     String pdfText = pdfToString();
@@ -20,9 +18,22 @@ public class App {
     int prevStartIndex = 0;
     // FIND INVOICE NUMBERS
     String invoiceNumber = "[0-9]{2}-[0-9]{8}";
+    // Line Number   \s[0-9]{3}\s
+
+    // Item Number   \s[0-9]{2}-[0-9]{5}\s
+
+    // Item Name   "(   \b([0-9]+\S.*?)|(   \b[a-zA-Z]+\S.*))\s+ "
+
+    // Ordered    (\d CS)   1
+    // Made    (\d CS)   2
+    // Scanned    (\d CS)   3
+
+
     Pattern invoiceNumPattern = Pattern.compile(invoiceNumber);
     Matcher invoiceNumMatcher = invoiceNumPattern.matcher(pdfText);
     ArrayList<String> lines = new ArrayList<>();
+    ArrayList<InvoiceLine> invoiceLines = new ArrayList<>();
+
     while (invoiceNumMatcher.find()) {
       //System.out.println("Found value start: " +
       // invoiceNumMatcher.start() );
@@ -32,7 +43,6 @@ public class App {
       // CURRENT START INDEX
       // Previous Start
 
-
       if (currentStartIndex != prevStartIndex) {
         currentStartIndex = invoiceNumMatcher.start();
         String sub = pdfText.substring(prevStartIndex,
@@ -40,14 +50,37 @@ public class App {
         );
         String[] strings = sub.split(" ");
         lines.add(sub);
-        System.out.println(strings);
 
-        InvoiceLine line = new InvoiceLine(strings[0], strings[2],
-                strings[4]);
-        // strings HOLDS AN ARRAY OF ALL THE VALUES IN BETWEEN TWO
-        // INVOICE NUMBERS
 
-        // ITEM NAME STARTS AT INDEX 15.
+        String invNum = "[0-9]{2}-[0-9]{8}";
+        Matcher invNumMatcher = Pattern.compile(invNum).matcher(sub);
+        boolean foundInvNum = invNumMatcher.find();
+        String invNumStr = invNumMatcher.group().trim();
+
+        String lineNum = "\\s[0-9]{3}\\s";
+        Matcher lineNumMatcher = Pattern.compile(lineNum).matcher(sub);
+        boolean foundLineNum = lineNumMatcher.find();
+        String lineNumStr = lineNumMatcher.group().trim();
+
+        String itemNum = "\\s[0-9]{2}-[0-9]{5}\\s";
+        Matcher itemNumMatcher = Pattern.compile(itemNum).matcher(sub);
+        boolean foundItemNum = itemNumMatcher.find();
+        String itemNumStr = itemNumMatcher.group().trim();
+
+        String itemName = "(   \\b([0-9]+\\S.*?)|(   \\b[a-zA-Z]+\\S.*))\\s+ ";
+        Matcher itemNameMatcher = Pattern.compile(itemName).matcher(sub);
+        boolean foundItemName = itemNameMatcher.find();
+        String itemNameStr = itemNameMatcher.group().trim();
+
+        //   ************* WORKING *************
+//        String lineNum = "\\s[0-9]{3}\\s";
+//        Pattern lineNumPat = Pattern.compile(lineNum);
+//        Matcher lineNumMatcher = lineNumPat.matcher(sub);
+//        boolean foundLineNo = lineNumMatcher.find();
+//        String matched = lineNumMatcher.group().trim();
+
+        InvoiceLine line = new InvoiceLine(invNumStr, lineNumStr, itemNumStr, itemNameStr);
+        invoiceLines.add(line);
 
 
       }
@@ -55,7 +88,7 @@ public class App {
 
 
     }
-    System.out.println(lines);
+    System.out.println(invoiceLines);
 
 //    String route = "[0-9]{2}-[0-9]{8}";
 //    Pattern routePattern = Pattern.compile(route);
--- src/main/java/org/cody/InvoiceLine.java
+++ src/main/java/org/cody/InvoiceLine.java
@@ -4,10 +4,12 @@ public class InvoiceLine {
   private String invNum;
   private String lineNum;
   private String itemNum;
+  private String itemName;
 
-  public InvoiceLine(String inv, String line, String item) {
+  public InvoiceLine(String inv, String line, String itemNum, String itemName) {
     this.invNum = inv;
-    //this.lineNum = line;
-    //this.itemNum = item;
+    this.lineNum = line;
+    this.itemNum = itemNum;
+    this.itemName = itemName;
   }
 }
